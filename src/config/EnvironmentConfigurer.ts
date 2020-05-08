f11a5fba86e3e60235b01d2dad3496f18cea66ef
Matheus <matheus.abreu@amedigital.com>
2020-05-08 10:20:11 -0300

    Adicionando novas variáveis ao EnvironmentConfigurer

--- src/config/EnvironmentConfigurer.ts
+++ src/config/EnvironmentConfigurer.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 export class EnvironmentConfigurer {
 
     private static validate = () => {
-        const variables: string[] = ['SERVER_PORT', 'DB_HOST', 'DB_PORT', 'DB_USERNAME', 'DB_PASSWORD'];
+        const variables: string[] = ['APP_HOST', 'APP_PORT', 'DB_HOST', 'DB_PORT', 'DB_USERNAME', 'DB_PASSWORD', 'LOG_LEVEL'];
         variables.forEach((variable) => {
             if (!process.env[variable]) throw new Error(`${variable} must be configured in environment!`);
         });
