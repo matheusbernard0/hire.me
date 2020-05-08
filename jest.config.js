5a89f9f202318582a797cd5858b992ace47a9b5b
Matheus <matheus.abreu@amedigital.com>
2020-05-08 11:24:24 -0300

    Ajuste no jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,43 +1,6 @@
 module.exports = {
-  // preset: 'ts-jest',
-  // testEnvironment: 'node',
-  // verbose: true,
-  // rootDir: './test',
-  //
-  // collectCoverage: true,
-  // coverageDirectory: './coverage',
-  // collectCoverageFrom: ['./src/**/*.ts'],
-
   preset: 'ts-jest',
-  verbose: true,
-  roots: [
-    '<rootDir>/src/',
-    '<rootDir>/test'
-  ],
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "ts",
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts}',
-    '!**/node_modules/**',
-    '!**/webpack**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
   testEnvironment: 'node',
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  transformIgnorePatterns: [
-    "<rootDir>/node_modules/"
-  ]
+  verbose: true,
+  rootDir: './test',
 };
